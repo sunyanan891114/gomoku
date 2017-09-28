75b20beb5e7c1574ac615d18242436cb5ef84d9f
sun.yanan <sun.yanan@rea-group.com>
2017-09-28 22:33:16 +0800

    Add transition to canvas background image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import Chessboard from './js/Chessboard';
 import {hideModal} from './js/modal';
 import './styles/styles.scss';
+import chessboardBackground from './asset/chessboard.jpg';
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById('single-player').addEventListener('click', startSingleGame);
   document.getElementById('multi-player').addEventListener('click', startMultipleGame);
+  showBackgroundAfterFullyLoaded();
 });
+
+const showBackgroundAfterFullyLoaded = () => {
+  const canvas = document.getElementById('chessboard-canvas');
+  canvas.style.backgroundImage = "none";
+  const bgImage = new Image();
+  bgImage.src = chessboardBackground;
+  bgImage.onload = function() {
+    canvas.style.backgroundImage = `url(${chessboardBackground})`;
+    canvas.style.filter = 'blur(0)';
+  };
+};
 
 const startSingleGame = () => {
   new Chessboard(15, 'single');
