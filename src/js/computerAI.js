4f2ad8f74ba3c50e8313e761aa64702d09641c7f
sun.yanan <sun.yanan@rea-group.com>
2017-09-28 18:56:30 +0800

    change AI back

--- src/js/computerAI.js
+++ src/js/computerAI.js
@@ -1,97 +1,104 @@
 import {calculateWinNums} from './winStrategy';
 
-const scoreWeight = {
-  player: [200, 400, 2000, 10000, 0],
-  computer: [220, 420, 2200, 20000, 0]
-};
-
 export default class ComputerAI {
   constructor(num) {
     const result = calculateWinNums();
     this.wins = result.wins;
     this.count = result.count;
     this.num = num;
-    this.playerScore = [];
-    this.computerScore = [];
-    this.playerWin = [];
-    this.computerWin = [];
   }
 
-  initScores() {
+  nextStep(chessboard) {
+    const myScore = [];
+    const computerScore = [];
+    const myWin = [];
+    const _myWin = [];
+    const computerWin = [];
+    const _compWin = [];
+    for (let i = 0; i < this.count; i++) {
+      myWin[i] = 0;
+      _myWin[i] = 0;
+      computerWin[i] = 0;
+      _compWin[i] = 0;
+    }
+    let max = 0, u = 0, v = 0;
     for (let i = 0; i < this.num; i++) {
-      this.playerScore[i] = [];
-      this.computerScore[i] = [];
+      myScore[i] = [];
+      computerScore[i] = [];
       for (let j = 0; j < this.num; j++) {
-        this.playerScore[i][j] = 0;
-        this.computerScore[i][j] = 0;
+        myScore[i][j] = 0;
+        computerScore[i][j] = 0;
       }
     }
 
-    for (let i = 0; i < this.count; i++) {
-      this.playerWin[i] = 0;
-      this.computerWin[i] = 0;
-    }
-  }
-
-  calculateWinStrategies(chessboard) {
     for (let i = 0; i < this.num; i++) {
       for (let j = 0; j < this.num; j++) {
         for (let k = 0; k < this.count; k++) { // 将可能赢的情况都加1
           if (this.wins[i][j][k]) {
-            if (chessboard[i][j] === 1) {
-              this.playerWin[k]++;
-              this.computerWin[k] = 5;//
-            } else if (chessboard[i][j] === 2) {
-              this.playerWin[k] = 5;
-              this.computerWin[k]++;//
+            if(chessboard[i][j] === 1) {
+              myWin[k]++;
+              computerWin[k] = 6;//
+            } else if(chessboard[i][j] === 2) {
+              myWin[k] = 6;
+              _compWin[k] = computerWin[k]; // 为悔棋做准备
+              computerWin[k]++;//
             }
           }
         }
       }
     }
-  }
-
-  calculateOptimizePosition(chessboard) {
-    let max = 0, u = 0, v = 0;
     for (let i = 0; i < this.num; i++) {
       for (let j = 0; j < this.num; j++) {
         if (chessboard[i][j] == 0) {
           for (let k = 0; k < this.count; k++) {
             if (this.wins[i][j][k]) {
-              this.playerScore[i][j] += scoreWeight.player[this.playerWin[k]];
-              this.computerScore[i][j] += scoreWeight.computer[this.computerWin[k]];
+              if (myWin[k] == 1) {
+                myScore[i][j] += 200;
+              } else if (myWin[k] == 2) {
+                myScore[i][j] += 400;
+              } else if (myWin[k] == 3) {
+                myScore[i][j] += 2000;
+              } else if (myWin[k] == 4) {
+                myScore[i][j] += 10000;
+              }
+
+              if (computerWin[k] == 1) {
+                computerScore[i][j] += 220;
+              } else if (computerWin[k] == 2) {
+                computerScore[i][j] += 420;
+              } else if (computerWin[k] == 3) {
+                computerScore[i][j] += 2100;
+              } else if (computerWin[k] == 4) {
+                computerScore[i][j] += 20000;
+              }
             }
           }
-          if (this.playerScore[i][j] > max) {
-            max = this.playerScore[i][j];
+
+          if (myScore[i][j] > max) {
+            max = myScore[i][j];
             u = i;
             v = j;
-          } else if (this.playerScore[i][j] == max) {
-            if (this.computerScore[i][j] > this.computerScore[u][v]) {
+          } else if (myScore[i][j] == max) {
+            if (computerScore[i][j] > computerScore[u][v]) {
               u = i;
               v = j;
             }
           }
 
-          if (this.computerScore[i][j] > max) {
-            max = this.computerScore[i][j];
+          if (computerScore[i][j] > max) {
+            max = computerScore[i][j];
             u = i;
             v = j;
-          } else if (this.computerScore[i][j] == max) {
-            if (this.playerScore[i][j] > this.playerScore[u][v]) {
+          } else if (computerScore[i][j] == max) {
+            if (myScore[i][j] > myScore[u][v]) {
               u = i;
               v = j;
             }
           }
+
         }
       }
     }
-    return {x: u, y: v};
-  }
-
-  nextStep(chessboard) {
-    this.initScores();
-    this.calculateWinStrategies(chessboard);
-    return this.calculateOptimizePosition(chessboard);
+    return {x: u, y: v}
   }
 }
